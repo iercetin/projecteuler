65868889bbb3045130fd820fa55eef03d9a5ddeb
ibrahim <iercetin44@gmail.com>
2020-04-18 03:52:12 +0300

    first commit, 43 solved, 43 solved

---
+++ 42/app.py
@@ -0,0 +1,30 @@
+import string
+letters = list(string.ascii_lowercase)
+
+
+def nthTriangle(n):
+	return int(n * (n+1) / 2)
+
+
+triangles = [nthTriangle(i) for i in range(1,100)]
+
+def wordVal(word):
+	v = 0
+	for w in word.lower():
+		v += letters.index(w) + 1
+	return v
+
+
+f = open("p042_words.txt").read()
+words = list(f.split(","))
+words = [w.replace("\"","") for w in words]
+
+
+winner = 0
+for word in words:
+	wordValue = wordVal(word)
+	if wordValue in triangles:
+		print("YES: Word: {}, Value: {}".format(word,str(wordValue)))
+		winner += 1
+
+print("Result: {}".format(str(winner)))
---
+++ 43/app.py
@@ -0,0 +1,38 @@
+import itertools
+
+numbers = "".join([str(i) for i in range(10)])
+
+def isPandigital(n):
+	for num in numbers:
+		if num not in n:
+			return False
+
+	for i in range(1,len(n)-2):
+		sub = int(n[i:(i+3)])
+
+		if i == 1 and sub % 2 != 0:
+			return False
+		if i == 2 and sub % 3 != 0:
+			return False
+		if i == 3 and sub % 5 != 0:
+			return False
+		if i == 4 and sub % 7 != 0:
+			return False
+		if i == 5 and sub % 11 != 0:
+			return False
+		if i == 6 and sub % 13 != 0:
+			return False
+		if i == 7 and sub % 17 != 0:
+			return False
+
+	return True
+
+nums = [str(i) for i in range(10)]
+
+winner = 0
+for ls in itertools.permutations(nums):
+	gl = "".join([str(it) for it in list(ls)])
+	if isPandigital(gl):
+		winner += int(gl)
+
+print(winner)
